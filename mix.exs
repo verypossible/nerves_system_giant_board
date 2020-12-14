1db73fc2c466be9a498a1771a023e129a05d566a
Justin Schneck <jschneck@mac.com>
2020-12-14 15:58:16 -0500

    Update system and example

--- mix.exs
+++ mix.exs
@@ -49,7 +49,7 @@ defmodule NervesSystemGiant.MixProject do
   defp deps do
     [
       {:nerves, "~> 1.6", runtime: false},
-      {:nerves_system_br, "1.11.4", runtime: false},
+      {:nerves_system_br, "1.13.4", runtime: false},
       {:nerves_toolchain_arm_unknown_linux_gnueabihf, "~> 1.3.0", runtime: false},
       {:nerves_system_linter, "~> 0.3.0", runtime: false},
       {:ex_doc, "~> 0.18", only: [:dev, :test], runtime: false}
